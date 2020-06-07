303982046c92e690d10940b4f021fa10725b6c52
ujjwal <ujjwalmaity98@gmail.com>
2020-06-07 12:56:31 +0530

    SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -7,7 +8,27 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('Hello World!'),
+        child: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 320.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('Singing Stars'),
+                background: Image.asset('images/image1.jpg', fit: BoxFit.cover),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                List.generate(
+                  30,
+                  (int index) => ListTile(title: Text("Item $index")),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
